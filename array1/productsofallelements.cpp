184175cdf7b3d78e1891337a4b9384bd4e45f315
dev_pranjal <118918821+AmazingPranjoel1024@users.noreply.github.com>
2024-01-31 20:34:31 +0530

    Add files via upload

---
+++ array1/productsofallelements.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(8);
+    v.push_back(9);
+
+    int product=1;
+    for(int i=0;i<v.size();i++){
+        product=product*v[i];
+
+    }
+    cout<<product;
+
+
+}

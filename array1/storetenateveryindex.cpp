ff698d9f67b1a95d645e0eaf2007c78ab628cd8b
dev_pranjal <118918821+AmazingPranjoel1024@users.noreply.github.com>
2024-04-30 22:52:48 +0530

    Add files via upload

---
+++ array1/storetenateveryindex.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int arr[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            arr[i][j]=10;
+        }
+    }
+    cout<<"Array of 10 :"<<endl;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            cout<<arr[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

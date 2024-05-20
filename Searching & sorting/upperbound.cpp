42c185131e8798116134875d66da962f51f4862b
dev_pranjal <118918821+AmazingPranjoel1024@users.noreply.github.com>
2024-05-20 21:27:36 +0530

    Add files via upload

---
+++ Searching & sorting/upperbound.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<climits>
+using namespace std;
+
+// int main(){
+//     int arr[11]={1,2,3,5,7,12,14,17,18,21,24};
+//     int n=11;
+//     int x=20;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]>x){
+//             cout<<arr[i];
+//             break;
+//         }
+//     }
+    
+// }
+
+
+
+int main(){
+    int arr[11]={1,2,3,5,7,12,14,17,18,21,24};
+    int n=11;
+    int x=20;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

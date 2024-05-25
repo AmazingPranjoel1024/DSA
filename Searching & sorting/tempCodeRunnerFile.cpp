33b225f7dcc3e9166122a1dd6978bf78e267456d
dev_pranjal <118918821+AmazingPranjoel1024@users.noreply.github.com>
2024-05-25 20:45:01 +0530

    Add files via upload

---
+++ Searching & sorting/tempCodeRunnerFile.cpp
@@ -0,0 +1,21 @@
+
+int main(){
+    int arr[]={3,6,5,1,2,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp =arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}

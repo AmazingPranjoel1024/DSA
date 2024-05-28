61861104883a04c089cc0117e415eca52f3103b0
dev_pranjal <118918821+AmazingPranjoel1024@users.noreply.github.com>
2024-05-28 21:15:07 +0530

    Add files via upload

---
+++ string/integertostring.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+//     int x=123456;
+//     string s= to_string(x);
+//     cout<<x;
+    int x=12345;
+    string s=to_string(x);
+    int z=s.length();
+    cout<<z;
+
+    
+
+
+
+}

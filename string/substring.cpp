d99a24a7775abd57fba281bb19ac0268fbcf5dc6
dev_pranjal <118918821+AmazingPranjoel1024@users.noreply.github.com>
2024-05-30 20:56:18 +0530

    Add files via upload

---
+++ string/substring.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    // string s="abcdefgh";
+    // cout<<s.substr(3);
+    string str;
+    cout<<"Enter string: ";
+    cin>>str;
+    int n=str.length();
+    cout<<str.substr(n/2);
+
+
+}
+
+
+
+
+

13a61b88e90b5c41cbfcc780ba202db52d73a360
dev_pranjal <118918821+AmazingPranjoel1024@users.noreply.github.com>
2024-05-31 22:27:16 +0530

    Add files via upload

---
+++ string/intro.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+// #include<string>
+using namespace std;
+
+int main(){
+    // char str[] = {'a','b','c','d','e'};
+    // // for(int i=0;i<5;i++){
+    // //     cout<<str[i]<<" ";
+    // // }
+    // // cout<<str;
+    // for(int i=0;str[i]!='\0';i++){
+    //     cout<<str[i]<<" ";
+    // }
+
+    // string s;
+    // // cin>>s;
+    // getline(cin,s);
+    // cout<<s;
+    string str="hello there this is programming";
+    cout<<str;
+    
+
+}
+
+
+
